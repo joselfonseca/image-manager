efdd6509f3a1b82353f4dbde0384d7d2e02bad19
jose fonseca <jose@ditecnologia.com>
2014-08-25 20:05:15 -0500

    Initial Commit

---
+++ src/Joselfonseca/ImageManager/ImageManagerServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Joselfonseca\ImageManager;
+
+use Illuminate\Support\ServiceProvider;
+
+class ImageManagerServiceProvider extends ServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot() {
+        $this->package('joselfonseca/image-manager');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
+        //
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides() {
+        return array();
+    }
+
+}
---
+++ src/migrations/2014_08_26_005557_image_manager_files_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ImageManagerFilesTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('image_manager_files', function($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('originalName');
+            $table->string('type');
+            $table->string('path');
+            $table->bigInteger('size');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('image_manager_files');
+    }
+
+}
