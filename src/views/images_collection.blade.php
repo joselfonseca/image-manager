88a43f6f16cc6b9c0dd048c71f05e56eace1cdac
Jose Luis Fonseca <desarrollo@reddesignsystems.com>
2015-05-19 15:25:44 -0500

    update pagination and change action for route

--- src/views/images_collection.blade.php
+++ src/views/images_collection.blade.php
@@ -2,9 +2,9 @@
     @foreach($files as $f)
     <div class='col-lg-4' id="imageManager_{{$f->id}}">
         <div class="thumbnail">
-            <img src="{{action('showthumb', $f->id)}}" alt="">
+            <img src="{{route('showthumb', $f->id)}}" alt="">
             <div class="caption">
-                <button class="btn btn-default" data-file-id='{{$f->id}}' data-preview-url="{{action('showthumb', $f->id)}}" data-action="selected-file"><i class="fa fa-check-circle"></i> Use</button>
+                <button class="btn btn-default" data-file-id='{{$f->id}}' data-preview-url="{{route('showthumb', $f->id)}}" data-action="selected-file"><i class="fa fa-check-circle"></i> Use</button>
                 <button type="button" class="btn btn-default popover-dismiss" data-container="body" data-toggle="popover" data-placement="top" data-content='Are you sure you want to delete the file?<br /><button class="btn btn-danger" data-file-id="{{$f->id}}" data-delete-url="{{action('ImageManagerDelete', $f->id)}}" data-action="delete-file"><i class="fa fa-trash-o"></i> Delete</button>'>
                     Delete File
                 </button>
@@ -15,6 +15,6 @@
 </div>
 <div class="row">
     <div class="center-block images-paginator" style="text-align: center">
-        {{$files->links()}}
+        {{$files->render()}}
     </div>
 </div>
